829d7e8dc689db94411e41d06f9b6112a7a955de
cln-clark <Clarknvls@gmail.com>
2025-01-30 19:52:48 +0800

    Removed code from assignment 1 and 2

--- assignments/assignments.controller.ts
+++ assignments/assignments.controller.ts
@@ -2,23 +2,6 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('assignments')
 export class AssignmentsController {
-  @Get('fibonacci/:n')
-  getFibonacciSequence(@Param('n') n: number): { sequence: number[] } {
-    const sequence = this.generateFibonacci(Number(n));
-    return { sequence };
-  }
-
-  private generateFibonacci(n: number): number[] {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
-
-    const sequence = [0, 1];
-    for (let i = 2; i < n; i++) {
-      sequence.push(sequence[i - 1] + sequence[i - 2]);
-    }
-    return sequence;
-  }
-
   @Get('prime/:number')
   checkPrime(@Param('number') number: number): { isPrime: boolean } {
     const isPrime = this.isPrime(Number(number));
